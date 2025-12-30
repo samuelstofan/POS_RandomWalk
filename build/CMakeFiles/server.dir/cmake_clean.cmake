4b9b2ba32e60f3efb175e68306c36db427f90fc8
Samuel <stofansamuel@gmail.com>
2025-12-30 14:57:34 +0100

    cmake

---
+++ build/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/link.d"
+  "CMakeFiles/server.dir/src/server.c.o"
+  "CMakeFiles/server.dir/src/server.c.o.d"
+  "server"
+  "server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
